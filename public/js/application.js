eb8420f7d8f5aba4b5296672529f9fc023dd6122
Daniel Lu <dandydanny@gmail.com>
2014-02-26 14:46:25 -0600

    Include local jQuery library and font

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  console.log("JS ready!");
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
