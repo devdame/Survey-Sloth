e7a3c64c6122cbaecec3790cb3320285b5604a6f
Daniel Lu <dandydanny@gmail.com>
2014-02-28 16:04:13 -0600

    Added 'survey has many responses through questions'

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,7 @@
 class Survey < ActiveRecord::Base
   belongs_to :user
   has_many :questions
+  has_many :responses, through: :questions
   has_many :completion_users
   has_many :survey_takers, through: :completion_users, class_name: "User"
 end
