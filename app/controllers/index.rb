1b475159cf108cb91987f1b54ad781a795a1ca9f
jdubnicek <jdubnicek@gmail.com>
2014-02-28 11:38:12 -0600

    initial routes, no methods

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,69 @@
+#***************************************************
+##USERS_____________________________________________
+#***************************************************
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+#-----------------------
+get '/sign_up' do
+	erb :sign_up
+end
+
+post '/sign_up' do
+	erb :sign_up
+end
+#-----------------------
+
+
+get '/sign_in' do 
+	erb :sign_in
+end
+
+post '/sign_in' do
+	erb :homepage
+end
+
+#-----------------------
+
+get 'view_profile' do
+	erb :view_profile
+end
+
+
+post 'view_profile' do
+	erb :view_profile
+end
+#-----------------------
+
+
+get '/create_survey' do
+	erb :create_survey
+end
+
+post '/create_survey' do
+	erb :create_survey
+end
+#-----------------------
+
+get '/logout' do
+	erb :index
+end
+
+
+#******************************************************
+##SURVEYS______________________________________________
+#******************************************************
+
+get '/browse_all' do
+	erb :browse_all
+end
+
+post '/browse_all' do
+	erb :browse_all
+end
+
+
